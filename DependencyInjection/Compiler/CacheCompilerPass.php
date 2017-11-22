1d1166481a5f57fbf2c51b6d497c3d3fed988692
Nacho Martín <nacho@limenius.com>
2017-11-22 21:15:14 +0100

    Cache support

---
+++ DependencyInjection/Compiler/CacheCompilerPass.php
@@ -0,0 +1,24 @@
+<?php
+namespace Limenius\ReactBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CacheCompilerPass implements CompilerPassInterface
+{
+    private $key;
+
+
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->getParameter('limenius_react.cache_enabled')) {
+            return;
+        }
+        $appCache = $container->findDefinition('cache.app');
+        $key = $container->getParameter('limenius_react.cache_key');
+        $renderer = $container
+            ->getDefinition('limenius_react.phpexecjs_react_renderer')
+            ->addMethodCall('setCache', [$appCache, $key])
+            ;
+    }
+}
