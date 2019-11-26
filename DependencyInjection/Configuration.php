79cc44e8f1f48498b09e3426a8deb22603060ef6
Melanie Kuhles <melanie.kuhles@quadriga.eu>
2019-11-26 10:33:29 +0100

    Fix Deprecated treeBuilder rootNode

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,7 +16,12 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('limenius_react');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('limenius_react');
+        }
         $rootNode
             ->children()
                 ->enumNode('default_rendering')
